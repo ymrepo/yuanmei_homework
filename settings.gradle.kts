1b9118cae703f3ebdb97a855c64cd8ff1b939e8c
Justin Honda <justinohonda@gmail.com>
2023-12-28 10:22:49 -0500

    Add schema

---
+++ settings.gradle.kts
@@ -0,0 +1,19 @@
+import java.net.URI
+
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "SWAPI GraphQL"
+include(":app")
